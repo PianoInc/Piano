76afc013eda6f0889ec900a68418b0906178fc7d
Kevin <Kevin_baba@naver.com>
2018-08-21 09:01:17 +0900

    make collectionView basic logic

---
+++ Piano/View/TitleView.swift
@@ -0,0 +1,21 @@
+//
+//  TitleView.swift
+//  Piano
+//
+//  Created by Kevin Kim on 2018. 8. 20..
+//  Copyright © 2018년 Piano. All rights reserved.
+//
+
+import UIKit
+
+class TitleView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
