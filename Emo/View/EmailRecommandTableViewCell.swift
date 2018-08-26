44943d9ff0dd78c352167e37c5772253faf55e87
Kevin <Kevin_baba@naver.com>
2018-08-26 17:08:00 +0900

    re update storyboard file

--- Emo/View/EmailRecommandTableViewCell.swift
+++ Emo/View/EmailRecommandTableViewCell.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class EmailRecommandTableViewCell: UITableViewCell {
 
-    @IBOutlet weak var nameLabel: UIStackView!
+    @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var bodyLabel: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
